61d98b71031a77606c38c7b1bcc05d77c1373386
Zhanna Kulagina <kuljeanne@gmail.com>
2023-09-21 11:15:57 +0300

    add img, base html, webpack conf

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const Webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = () => {
+  const config = {
+    entry: ['./src/styles/styles.scss'],
+    output: {
+      path: path.join(__dirname, '/dist'),
+      clean: true,
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 8080,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/,
+          type: 'asset/resource',
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+      }),
+      new CopyPlugin({
+        patterns: [{
+          from: './public/img',
+          to: './img',
+          noErrorOnMissing: true,
+        }],
+      }),
+    ]
+  }
+
+  return config
+}
